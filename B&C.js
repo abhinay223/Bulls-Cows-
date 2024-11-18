0d9f553a11eb88d90ffa9108aa9551b6b7d5bc1a
abhinay223 <136807425+abhinay223@users.noreply.github.com>
2024-11-18 23:57:42 +0530

    Update B&C.js

--- B&C.js
+++ B&C.js
@@ -1,6 +1,14 @@
-
+var secretCode = '0000';
+var input1 = '';
+var x = document.getElementById("guess-section");
+x.style.display = 'none';
+var y = document.getElementById("reset-div");
+y.style.display = 'none';
 
 function generateSecretCode() {
+
+    var z = document.getElementById('new-game');
+  z.style.display = 'none';
     let code = '';
     while (code.length < 4) {
         const digit = Math.floor(Math.random() * 10);
@@ -15,12 +23,14 @@ function generateSecretCode() {
 function calculateBullsAndCows(guess, secretCode) {
     let bulls = 0;
     let cows = 0;
-   
+  
     
     let secretArray = secretCode.split('');
-    
+   
+
     // Count bulls
-    var input1 = document.getElementById('guess-input');
+     input1 = document.getElementById('guess-input');
+    input1.disable = true;
     var input2 = input1.value;
     
     for (let i = 0; i < 4; i++) {
@@ -43,14 +53,12 @@ function calculateBullsAndCows(guess, secretCode) {
 }
 
 function playGame() {
-    
-   
-    const secretCode = generateSecretCode();
+
+    // const secretCode = generateSecretCode();
     
     let attempts = 0;
 
-    console.log("Welcome to Bulls and Cows!");
-    console.log("Try to guess the 4-digit number with no repeating digits.");
+  
 
    
     function askQuestion() {
@@ -66,6 +74,8 @@ function playGame() {
             
             if (input.length !== 4 || isNaN(guess1) || new Set(guess1).size !== 4) {
                 console.log('Invalid guess. Please enter a 4-digit number with no repeated digits.');
+                alert('Number is invalid, Please enter the number');
+               
                 return;
             }
             
@@ -80,9 +90,9 @@ function playGame() {
                 console.log(`Congratulations! You guessed the number in ${attempts} attempts.`);
                 rl.close();
             } else {
-             
+               
             }
-       
+    
         let outBulls = bulls ;
         document.getElementById("outbulls").innerHTML = outBulls; 
 
@@ -92,3 +102,21 @@ function playGame() {
 
    askQuestion();
 }
+
+function newGame(){
+    console.log("Welcome to Bulls and Cows!");
+    console.log("Try to guess the 4-digit number with no repeating digits.");
+    secretCode = generateSecretCode();
+    x.style = 'block';
+  
+  y.style = 'block';
+    console.log('secret code'+secretCode);
+
+    document.getElementById("outbulls").innerHTML = 0; 
+    document.getElementById("outcows").innerHTML = 0; 
+
+    
+
+}
+
+
